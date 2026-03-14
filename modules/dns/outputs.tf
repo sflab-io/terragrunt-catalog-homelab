6067cb3178bdb58bb762fd296da98af10037ddc6
Sebastian Freund <abes140377@web.de>
2026-03-14 09:16:28 +0100

    chore: test fix

--- modules/dns/outputs.tf
+++ modules/dns/outputs.tf
@@ -1,11 +1,11 @@
 output "fqdn" {
   description = "Fully qualified domain name of the normal DNS record. Null if normal record is not created."
-  value       = length(dns_a_record_set.normal) > 0 ? "${dns_a_record_set.normal[0].name}.${dns_a_record_set.normal[0].zone}" : null
+  value       = length(dns_a_record_set.normal) > 0 ? "${dns_a_record_set.normal[0].name}.${trimsuffix(dns_a_record_set.normal[0].zone, ".")}" : null
 }
 
 output "fqdn_wildcard" {
   description = "Fully qualified domain name of the wildcard DNS record. Null if wildcard record is not created."
-  value       = length(dns_a_record_set.wildcard) > 0 ? "${dns_a_record_set.wildcard[0].name}.${dns_a_record_set.wildcard[0].zone}" : null
+  value       = length(dns_a_record_set.wildcard) > 0 ? "${dns_a_record_set.wildcard[0].name}.${trimsuffix(dns_a_record_set.wildcard[0].zone, ".")}" : null
 }
 
 output "addresses" {
